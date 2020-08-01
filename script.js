3ef3696e944aa92b44c930c7518893c28998d406
Joe Lynn <josephlynn94@gmail.com>
2020-08-01 12:51:27 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+const addButton = document.querySelector('.addButton')
+const container = document.querySelector('.container')
+const inputValue = document.querySelector('.input')
+const editButton = document.querySelectorAll('.editButton')
+const deleteButton = document.querySelectorAll('.deleteButton')
+
+
+
+
